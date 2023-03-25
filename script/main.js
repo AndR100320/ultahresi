fc85fcb66be156254d64fb76b175f1085fb3bbd5
Andri Febriawan <111458135+AndR100320@users.noreply.github.com>
2023-03-26 01:19:44 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,14 +1,14 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
   Swal.fire({
-    title: 'Do you want to play music in the background?',
+    title: 'Ini isinya pake b.inggris ya,, ehh mau pake musik ga nihh?',
     // text: "You won't be able to revert this!",
     icon: 'warning',
     showCancelButton: true,
     confirmButtonColor: '#3085d6',
     cancelButtonColor: '#d33',
-    confirmButtonText: 'Yes',
-    cancelButtonText: 'No',
+    confirmButtonText: 'iyah',
+    cancelButtonText: 'engga',
   }).then((result) => {
     if (result.isConfirmed) {
       document.querySelector('.song').play();
